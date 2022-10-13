4cbef43a95eaf2b2b3bade2ae1b257e5cb1e4ccd
bosnik <rodion1991313@gmail.com>
2022-10-13 16:51:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
            $('.overlay, #order').fadeOut('slow');
             
         }
-    });
+    };
 
     function validateForm(form) {
         $(form).validate({
